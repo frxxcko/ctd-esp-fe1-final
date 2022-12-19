35c558de9aaacddacb606e85a219839f08986616
Juan_Basualdo <juan.basualdo@incluit.com>
2022-12-19 16:44:10 -0300

    cambio de extension de jsx a tsx

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+import PaginaInicio from "./paginas/Inicio.pagina";
+import PaginaFavoritos from "./paginas/Favoritos.pagina";
+import Encabezado from "./componentes/layout/encabezado.componente";
+import { Provider } from 'react-redux';
+import { store } from "./redux/store";
+import PaginaDetalle from "./paginas/Detalle.pagina";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Encabezado />
+        <Routes>
+          <Route path="/" element={<PaginaInicio />} />
+          <Route path="favoritos" element={<PaginaFavoritos />} />
+          <Route path="detalle" element={<PaginaDetalle />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
